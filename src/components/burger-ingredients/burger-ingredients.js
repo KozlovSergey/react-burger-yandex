525acbb34f8065895c85526ed88d6316dc2fe58c
skozlov <sergey.kozlov@icertainty.com>
2022-11-13 18:06:55 +0400

    add components

---
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./burger-ingredients.module.css";
+import IngredientItem from "../ingredient-item/ingredient-item";
+import data from "../../utils/data.json";
+
+const BurgerIngredients = () => {
+  const renderElements = (data, category) => {
+    const type =
+      (category === "Булки" && "bun") ||
+      (category === "Соусы" && "sauce") ||
+      (category === "Начинки" && "main");
+    const result = data.map((e) => {
+      return (
+        e.type === type && (
+          <IngredientItem
+            key={e._id}
+            name={e.name}
+            price={e.price}
+            img={e.image_large}
+          />
+        )
+      );
+    });
+    return (
+      <>
+        {category === "Булки" ? (
+          <h2 className="text text_type_main-medium">{category}</h2>
+        ) : (
+          <h2 className="text text_type_main-medium mt-10">{category}</h2>
+        )}
+
+        <div className={styles.items}>{result}</div>
+      </>
+    );
+  };
+  const [current, setCurrent] = React.useState("one");
+  return (
+    <div className={styles.warp}>
+      <h1 className="text text_type_main-large mt-10">Соберите бургер</h1>
+      <div className={styles.main}>
+        <div className="mt-5 mb-10" style={{ display: "flex" }}>
+          <Tab value="one" active={current === "one"} onClick={setCurrent}>
+            Булки
+          </Tab>
+          <Tab value="two" active={current === "two"} onClick={setCurrent}>
+            Соусы
+          </Tab>
+          <Tab value="three" active={current === "three"} onClick={setCurrent}>
+            Начинки
+          </Tab>
+        </div>
+        <div className={styles.scroll}>
+          {renderElements(data, "Булки")}
+          {renderElements(data, "Соусы")}
+          {renderElements(data, "Начинки")}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BurgerIngredients;
---
+++ src/components/ingredient-item/ingredient-item.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Counter,
+  CurrencyIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./ingredient-item.module.css";
+
+const IngredientItem = (props) => {
+
+  return (
+    <div className={styles.block}>
+      <Counter  count={1} size="default" />
+      <img alt={props.name} src={props.img} className={styles.img} />
+      <div className={styles.price}>
+        <p className="text text_type_digits-default pr-2">{props.price}</p>
+        <CurrencyIcon type="primary" />
+      </div>
+      <div className={styles.name}>
+        <p className="mt-3 text text_type_main-default">{props.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default IngredientItem;
